6dfbe1c4ab71d7c216fb2bcff1b328647da21993
manginoja <36826929+manginoja@users.noreply.github.com>
2022-06-20 18:02:44 -0700

    Update newPike.js

--- newPike.js
+++ newPike.js
@@ -1,4 +1,4 @@
-import * as THREE from './three/build/three.module.js';/*
+/*import * as THREE from './three/build/three.module.js';
 import { MapControls }  from './three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 
